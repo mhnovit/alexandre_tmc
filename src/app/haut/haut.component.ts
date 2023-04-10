d785d967f34843920db67d5963d94aa8acd57611
mhatchi <michael.hatchi@novatlas.com>
2023-04-10 12:18:42 +0200

    header

---
+++ src/app/haut/haut.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-haut',
+  templateUrl: './haut.component.html',
+  styleUrls: ['./haut.component.scss']
+})
+export class HautComponent {
+
+}
